a075254b5856a388e8369100d0ecf41b1ccd4f03
MachineMitch21 <mitchschutt@outlook.com>
2018-04-02 00:41:37 -0400

    Added ConsoleStream

---
+++ MiscClassStuff/build/CMakeFiles/classes.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/Code/C++Tests/MiscClassStuff/ConsoleStream.cpp" "F:/Code/C++Tests/MiscClassStuff/build/CMakeFiles/classes.dir/ConsoleStream.cpp.obj"
+  "F:/Code/C++Tests/MiscClassStuff/ConsoleStreamListener.cpp" "F:/Code/C++Tests/MiscClassStuff/build/CMakeFiles/classes.dir/ConsoleStreamListener.cpp.obj"
+  "F:/Code/C++Tests/MiscClassStuff/FileStream.cpp" "F:/Code/C++Tests/MiscClassStuff/build/CMakeFiles/classes.dir/FileStream.cpp.obj"
+  "F:/Code/C++Tests/MiscClassStuff/FileStreamListener.cpp" "F:/Code/C++Tests/MiscClassStuff/build/CMakeFiles/classes.dir/FileStreamListener.cpp.obj"
+  "F:/Code/C++Tests/MiscClassStuff/Stream.cpp" "F:/Code/C++Tests/MiscClassStuff/build/CMakeFiles/classes.dir/Stream.cpp.obj"
+  "F:/Code/C++Tests/MiscClassStuff/StreamListener.cpp" "F:/Code/C++Tests/MiscClassStuff/build/CMakeFiles/classes.dir/StreamListener.cpp.obj"
+  "F:/Code/C++Tests/MiscClassStuff/main.cpp" "F:/Code/C++Tests/MiscClassStuff/build/CMakeFiles/classes.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MiscClassStuff/build/CMakeFiles/classes.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/classes.dir/ConsoleStream.cpp.obj"
+  "CMakeFiles/classes.dir/ConsoleStreamListener.cpp.obj"
+  "CMakeFiles/classes.dir/FileStream.cpp.obj"
+  "CMakeFiles/classes.dir/FileStreamListener.cpp.obj"
+  "CMakeFiles/classes.dir/Stream.cpp.obj"
+  "CMakeFiles/classes.dir/StreamListener.cpp.obj"
+  "CMakeFiles/classes.dir/main.cpp.obj"
+  "classes.pdb"
+  "classes.exe"
+  "classes.exe.manifest"
+  "libclasses.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/classes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
